4a27dd2801514cc35bd83501425766f52d039755
Renato <renatosilsoa@gmail.com>
2026-09-07 18:13:53 -0300

    Listando Perguntas

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,33 +1,33 @@
-import { Request, Response } from "express"
-import {prisma} from "@/prisma"
+import { Request, Response } from "express";
+import { prisma } from "@/prisma";
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+    const questions = await prisma.question.findMany();
+    return response.json(questions);
   }
 
   async create(request: Request, response: Response) {
-
-    const {title, content, user_id} = request.body
+    const { title, content, user_id } = request.body;
 
     await prisma.question.create({
       data: {
         title,
         content,
-        userId: user_id
-      }
-    })
+        userId: user_id,
+      },
+    });
 
-    return response.status(201).json({message:"ok"})
+    return response.status(201).json({ message: "ok" });
   }
 
   async update(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async remove(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 }
 
-export { QuestionsController }
+export { QuestionsController };
